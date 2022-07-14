eb59a90bc9d7522ab04776d20f14dee0d72d716a
Rob Davis <robertpdavis@optusnet.com.au>
2022-07-14 17:18:40 +1000

    Added profile links to footer and styled.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,15 @@ import React from 'react';
 import '../app.css';
 import github from '../assets/images/github.png';
 import linkedin from '../assets/images/linkedIn.png';
+import stack from '../assets/images/stackoverflow.png';
 
 function Footer() {
   return (
     <footer>
-      <section className="contact">
-        <div className="contact-item">
-          <a href="https://github.com/robertpdavis" target="_blank" rel="noreferrer"><img src={github} alt="Guthub" /></a>
-          <a href="https://linkedin.com/in/robdavisaus" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn" /></a>
-          <a href="./public/docs/resume.pdf" target="_blank">My Resume</a>
-        </div>
+      <section className="profile-links">
+        <a href="https://github.com/robertpdavis" target="_blank" rel="noreferrer"><img src={github} alt="Guthub" /></a>
+        <a href="https://linkedin.com/in/robdavisaus" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn" /></a>
+        <a href="https://stackoverflow.com/users/18016811/rob-davis" target="_blank"><img src={stack} alt="Stackoverflow" /></a>
       </section>
     </footer>
   );
